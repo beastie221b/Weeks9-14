f2440025fe90c72b15e7c19bb3e8efc8bb268e3c
beastie221b <zyw125221@gmail.com>
2025-04-09 21:37:09 -0400

    Added a game over screen, a rolling animation, and some particle effects.

--- Assets/System Assignment/Scripts/Penguin.cs
+++ Assets/System Assignment/Scripts/Penguin.cs
@@ -15,11 +15,13 @@ public class Penguin : MonoBehaviour
     public Slider healthBar;
     public TMP_Text score;
     int currentScore = 0;
+    public GameObject gameOverScreen;
 
     void Start()
     {
         // In default, the player can be damaged.
         penguinEvent.AddListener(Damage);
+        gameOverScreen.SetActive(false);
     }
 
     void Update()
@@ -34,6 +36,12 @@ public class Penguin : MonoBehaviour
 
         healthBar.value = health;
         score.text = "SCORE: " + currentScore.ToString();
+
+        // Set the game over screen on when the health is running out.
+        if (health == 0)
+        {
+            gameOverScreen.SetActive(true);
+        }
     }
 
     void Roll()
@@ -44,6 +52,22 @@ public class Penguin : MonoBehaviour
         // Start rolling coroutine to end rolling.
         RollingCoroutine = RollEnd();
         StartCoroutine(RollingCoroutine);
+        // Start roll animation coroutine.
+        RollingCoroutine = RollAnimation();
+        StartCoroutine(RollAnimation());
+    }
+
+    IEnumerator RollAnimation()
+    {
+        float numOfFrames = IFrameSpeed / Time.deltaTime;
+        float angle = 360 / numOfFrames;
+        // Rotate 360 degree in IFrame speed seconds.
+        for (int i = 0; i < numOfFrames; i++)
+        {
+            transform.Rotate(new Vector3(0, 0, -angle));
+            yield return null;
+        }
+        transform.rotation = Quaternion.identity;
     }
     
     IEnumerator RollEnd()
